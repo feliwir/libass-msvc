f0241720f006739a023ac28a05bd48129b323735
feliwir <stephan.vedder@gmail.com>
2015-10-07 16:30:56 +0200

    Fixed asm optimizations

--- libass-0.13.0/libass/config.h
+++ libass-0.13.0/libass/config.h
@@ -2,5 +2,11 @@
 #define _CRT_NONSTDC_NO_DEPRECATE
 
 #define CONFIG_DIRECTWRITE	1
-#define CONFIG_ASM			1
 #define CONFIG_RASTERIZER	1
+#define CONFIG_ASM			1
+
+#ifdef _M_IX86
+#define __i386__
+#elif _M_AMD64
+#define __x86_64__
+#endif
